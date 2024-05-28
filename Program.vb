dfbc4a77978240baab60a697c10e2e1355f005b9
User <Willspri@gmail.com>
2024-05-28 15:31:15 +1200

    committed

---
+++ Program.vb
@@ -0,0 +1,23 @@
+Imports System.IO
+Imports System.Runtime.CompilerServices
+
+Module WriteToFile
+	Sub Main()
+		'Promt to enter text
+		Console.Write("Enter text to store in file: ")
+		Dim userInput As String = Console.ReadLine()
+		' Full path
+		Dim filePath As String = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "output.txt")
+
+		' Write input to file
+		Try
+			File.WriteAllText(filePath, userInput)
+			Console.WriteLine("Saved successfully")
+		Catch ex As Exception
+			Console.WriteLine("Error writing to file: " & ex.Message)
+		End Try
+		' Wait for keypress before closing
+		Console.ReadKey()
+
+	End Sub
+End Module
